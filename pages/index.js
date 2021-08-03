edf1b66eba1049ed78fe28204ad67a4d74c7b7a6
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-08-03 12:03:40 +0700

    generate test

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,145 @@
-import Head from 'next/head'
+import { has, isPlainObject, keys, values } from 'lodash';
+import Head from 'next/head';
+import { Component } from 'react';
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '{"message": "hello"}',
+      output: '',
+    };
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  generateTests = () => {
+    this.state.output = '';
+    const input = JSON.parse(this.state.value);
+
+    if (Array.isArray(input)) {
+      alert('Array is not supported yet :(');
+    }
+
+    if (!isPlainObject(input)) {
+      alert(`This data type of ${typeof input} is not supported yet :(`);
+    }
+
+    const vals = values(input);
+    vals.forEach((val) => {
+      if (typeof val === 'object') {
+        alert(`This data type of ${typeof val} is not supported yet :(`);
+      }
+    });
+
+    // this.state.output += 'is object\n';
+    this.state.output += `
+      pm.test('response body should be an object', () => {
+        const responseJson = pm.response.json();
+        pm.expect(responseJson).to.be.an('object');
+      });
+      \n\n`;
+
+    const properties = keys(input);
+    properties.forEach((property) => {
+      // this.state.output += `has ${property}\n`;
+      this.state.output += `
+      pm.test('response body should have the correct property of "${property}"', () => {
+        const responseJson = pm.response.json();
+
+        pm.expect(responseJson).to.have.ownProperty('${property}');
+      });
+      \n\n`;
+
+      const datatype = typeof input[property];
+      // this.state.output += `property ${property} is ${datatype}\n`;
+      this.state.output += `
+      pm.test('response body should have the correct data type for "${property}"', () => {
+        const responseJson = pm.response.json();
+
+        pm.expect(responseJson.${property}).to.be.an('${datatype}');
+        pm.expect(responseJson.${property}).not.eq(undefined);
+        pm.expect(responseJson.${property}).not.eq(null);
+      });
+      \n\n`;
+
+      if (input[property]) {
+        // this.state.output += `property ${property} has value of \"${input[property]}\"\n`;
+        this.state.output += `
+        pm.test('response body should have the correct and value for "${property}"', () => {
+          const responseJson = pm.response.json();
+
+          pm.expect(responseJson.${property}).to.equals('${input[property]}');
+        });
+        \n\n`;
+      }
+    });
+
+    console.log(this.state.output);
+  };
+
+  render() {
+    const { output } = this.state;
+
+    return (
+      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <Head>
+          <title>Postman Test Generator</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+
+        <main className='flex flex-col items-center flex-1 w-full px-4 text-center md:px-20 md:justify-center'>
+          <h1 className='text-4xl font-bold md:text-6xl'>
+            Postman Test Generator
+          </h1>
+
+          <p className='mt-3 text-2xl'>Generate test case by json response.</p>
+
+          <div className='mt-6'>
+            <h2>Step 1: Paste your response here</h2>
+            <textarea
+              value={this.state.value}
+              onChange={this.handleChange}
+              className='w-full mt-4 border'
+              rows='5'
+              placeholder="{'message': 'Hello World'}"></textarea>
+          </div>
+          <button
+            className='px-4 py-2 mt-6 border'
+            onClick={this.generateTests}>
+            Generate test cases 🎆
+          </button>
+          <div className='mt-6'>
+            <h2>Step 2: Copy this tests to Postman {output}</h2>
+            {/* <p>{this.state.value}</p>
+            <p>{this.state.output}</p> */}
+            <textarea className='mt-4' value={output} readOnly></textarea>
+          </div>
+        </main>
+
+        <footer className='flex items-center justify-center w-full h-24 border-t'>
           <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
+            className='flex items-center justify-center'
+            href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'>
+            Powered by{' '}
+            <img src='/vercel.svg' alt='Vercel Logo' className='h-4 ml-2' />
           </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+        </footer>
+      </div>
+    );
+  }
 }
+
+export default Home;
